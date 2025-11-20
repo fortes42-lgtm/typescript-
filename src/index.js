15dcbe12218d111425c04477de367bb211a22cb8
fortes42 <forteslv42@gmail.com>
2025-11-20 16:28:08 +0900

    value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-console.log("Hello, TypeScript!");
-var nums = [];
-var a = 10 + nums[0];
+// 기본 타입 템플릿
+// 1. string
+// let myString: string;
+// console.log("String:", myString);
+// // 2. number
+// let myNumber: number;
+// console.log("Number:", myNumber);
+// // 3. boolean
+// let myBoolean: boolean;
+// console.log("Boolean:", myBoolean);
+// // 4. null
+// let myNull: null;
+// console.log("Null:", myNull);
+// // 5. any
+// let myAny: any;
+// console.log("Any string:", myAny);
+// // myAny 값 변경
+// console.log("Changed to number:", myAny);
+// // myAny 값 변경
+// console.log("Changed to boolean:", myAny);
+var greeting = "Halo Dunia!";
+console.log("String:", greeting);
+var year = 2025;
+console.log("Number:", year);
+var isNice = true;
+console.log("Boolean:", isNice);
+var childrenNum = null;
+console.log("Null:", childrenNum);
+var anyValue = "Ini adalah string dengan tipe any.";
+console.log("Any string:", anyValue);
+anyValue = 42;
+console.log("Changed to number:", anyValue);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
-console.log("Hello, TypeScript!");
+// 기본 타입 템플릿
 
-const nums: number[] = [];
+// Declare minimal console shape so TypeScript can compile without lib changes
+declare var console: { log: (...args: any[]) => void };
 
-const a = 10 + nums[0];
+// 1. string
+// let myString: string;
+// console.log("String:", myString);
+
+// // 2. number
+// let myNumber: number;
+// console.log("Number:", myNumber);
+
+// // 3. boolean
+// let myBoolean: boolean;
+// console.log("Boolean:", myBoolean);
+
+// // 4. null
+// let myNull: null;
+// console.log("Null:", myNull);
+
+// // 5. any
+// let myAny: any;
+// console.log("Any string:", myAny);
+
+// // myAny 값 변경
+// console.log("Changed to number:", myAny);
+
+// // myAny 값 변경
+// console.log("Changed to boolean:", myAny);
+
+const greeting: string = "Halo Dunia!";
+console.log("String:", greeting);
+
+const year: number = 2025;
+console.log("Number:", year);
+
+const isNice: boolean = true;
+console.log("Boolean:", isNice);
+
+const childrenNum: null = null;
+console.log("Null:", childrenNum);
+
+let anyValue: any = "Ini adalah string dengan tipe any.";
+console.log("Any string:", anyValue);
+anyValue = 42;
+console.log("Changed to number:", anyValue);
